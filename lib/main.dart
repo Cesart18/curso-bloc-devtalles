a6d7c54cc9c575f65d483e6440b6965077ea7096
Cesar Tovar <cjataonline@gmail.com>
2024-04-27 05:29:04 -0400

    01-simple_cubit finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
+import 'package:blocs_app/presentation/blocs/blocs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:blocs_app/config/config.dart';
 
-void main() => runApp(const MyApp());
+void main(){
+
+  runApp(const BlocsProvider());
+}
+
+class BlocsProvider extends StatelessWidget {
+  const BlocsProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+          BlocProvider(create: (context) => UsernameCubit(), lazy: false,)
+      ],
+       child: const MyApp());
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/presentation/screens/01_simple_cubit/cubit_screen.dart
+++ lib/presentation/screens/01_simple_cubit/cubit_screen.dart
@@ -1,17 +1,32 @@
+import 'package:blocs_app/config/config.dart';
+import 'package:blocs_app/presentation/blocs/blocs.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CubitScreen extends StatelessWidget {
   const CubitScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // final usernameCubit = context.watch<UsernameCubit>();
+    final textStyle = Theme.of(context).textTheme;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cubit'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+          child: BlocBuilder<UsernameCubit, String>(
+            buildWhen: (previous, current) => previous != current,
+        builder: (context, state) => Text(state, style: textStyle.titleLarge,),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // ! por lo general se usa el read dentro de una funcion, es parecido a lo que es el riverpod
+          context
+              .read<UsernameCubit>()
+              .setUsername(RandomGenerator.getRandomName());
+        },
+        child: const Icon(Icons.refresh),
       ),
     );
   }
